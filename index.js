f29ff60c911bb0a902ae9d1bbc2334ce3c1e6049
CK7373 <claire.kariuki@student.moringaschool.com>
2025-03-04 11:19:20 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+
+const currentUser = "Ruffles";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+
+console.log(welcomeMessage)
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser} !`
+
+const firstInitial = currentUser[0];
+
+const restOfName = currentUser.slice(1);
+
+console.log(excitedWelcomeMessage)
+
+const shortGreeting =`Welcome, ${firstInitial} !`
+
+console.log(shortGreeting)
+
+console.log(welcomeMessage)
+
+
+
+
+
